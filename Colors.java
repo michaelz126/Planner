9fce688bbe0991f3687a856aed06a338eaf6d5ea
michaelz126 <1zhang.michael@gmail.com>
2025-06-04 02:22:27 +0000

    final touches

--- Colors.java
+++ Colors.java
@@ -2,16 +2,11 @@ public class Colors {
     public static String RESET = "\u001B[0m";
     public static String RED = "\u001B[31m";
     public static String GREEN = "\u001B[32m";
-    public static String YELLOW = "\u001B[33m";
     public static String BLUE = "\u001B[34m";
-    public static String WHITE = "\u001B[37m";
     public static String ORANGE = "\u001B[38;5;208m";
-    public static String NAVY = "\u001B[38;5;21m";
     public static String LIGHTBLUE = "\u001B[38;5;45m";
     public static String BOLDRED = "\u001B[1m\u001B[31m"; // Bold Red
-    public static String DARKRED = "\u001B[38;5;88m"; // Dark Red
     public static String BOLD = "\u001B[1m"; // Bold
-    public static final String ANSI_UNDERLINE = "\u001B[4m"; 
 
 }
 
